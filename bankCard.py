b946a1b4a5266c00c0fa7c2c26c9994cc8aa9702
Bawaaaaah <limason51@gmail.com>
2014-06-06 10:48:02 +0200

    now use a class and add some credit card template

--- bankCard.py
+++ bankCard.py
@@ -3,34 +3,49 @@
 from random import SystemRandom
 
 
-def luhn(cardNumber):
-    result = 0
-    cardNumber = str(cardNumber)
-    tmp = (len(cardNumber) % 2 == 1)
-    for n in cardNumber:
-        n = int(n)
-        tmp = not tmp
-        if(tmp):
-            result += n*2 - (0 if n < 5 else 9)
+class bankCard:
+
+    @staticmethod
+    def luhn(cardNumber):
+        result = 0
+        cardNumber = str(cardNumber)
+        tmp = (len(cardNumber) % 2 == 1)
+        for n in cardNumber:
+            n = int(n)
+            tmp = not tmp
+            if(tmp):
+                result += n*2 - (0 if n < 5 else 9)
+            else:
+                result += n
+        return result
+
+    @staticmethod
+    def checkCardNumber(cardNumber):
+        return ((bankCard.luhn(cardNumber) % 10) == 0)
+
+    @staticmethod
+    def genCardNumber(start="", length=16):
+        start = str(start)
+        for n in range(len(start), length-1):
+            start += str(SystemRandom().randint(0, 9))
+        tmp = (bankCard.luhn(start+"0") % 10)
+        if tmp == 0:
+            start += "0"
         else:
-            result += n
-    return result
+            start += str(10 - tmp)
+        return start
 
+    @staticmethod
+    def MASTER_CARD():
+        return (SystemRandom().randint(50, 55), 16)
 
-def checkCardNumber(cardNumber):
-    return ((luhn(cardNumber) % 10) == 0)
+    @staticmethod
+    def AMERICAN_EXPRESS():
+        return (34, 15) if SystemRandom().randint(0, 1) else (37, 15)
 
-
-def genCardNumber(start="", length=16):
-    start = str(start)
-    for n in range(len(start), length-1):
-        start += str(SystemRandom().randint(0, 9))
-    tmp = (luhn(start+"0") % 10)
-    if tmp == 0:
-        start += "0"
-    else:
-        start += str(10 - tmp)
-    return start
+    @staticmethod
+    def VISA():
+        return (4, 13) if SystemRandom().randint(0, 1) else (4, 16)
 
 
 if __name__ == "__main__":
@@ -38,6 +53,9 @@ if __name__ == "__main__":
     to see Issuer identification number go to
     http://en.wikipedia.org/wiki/Credit_card_number#Issuer_identification_number_.28IIN.29
     """
-    print(genCardNumber(51)) # master card
-    print(genCardNumber(37, 15)) # american express
-    print(luhn("4417123456789113"))
+    print("Master Card " + bankCard.genCardNumber(51))  # master card
+    print("American Express " + bankCard.genCardNumber(37, 15))  # american express
+    print("Master Card " + bankCard.genCardNumber(*bankCard.MASTER_CARD()))
+    print("American Express " + bankCard.genCardNumber(*bankCard.AMERICAN_EXPRESS()))
+    print("Visa " + bankCard.genCardNumber(*bankCard.VISA()))
+    print(bankCard.luhn("4417123456789113"))
